348806c2b2c7a8c1e9c7176151dfd4eea87d6c28
memarze <memarez@gmail.com>
2026-01-05 20:22:22 -0600

    first comment

---
+++ src/main/java/com/novus/novusbank/NovusbankApplication.java
@@ -0,0 +1,13 @@
+package com.novus.novusbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovusbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovusbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novus/novusbank/NovusbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novus.novusbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovusbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
